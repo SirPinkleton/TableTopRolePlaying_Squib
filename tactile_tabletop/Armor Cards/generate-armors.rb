631b6f9c9f25a410e262d1d9a5e2ebdfd909c764
Jason Nelson <jason.andr.nelson@gmail.com>
2023-01-21 23:10:26 -0800

    giving the same treatment to armors as I did weapons: adding new .yml for sprue (referenced in .rb file), adding the generate cards .ps1 script. adjusted numbers to suit the smaller armor cards

--- tactile_tabletop/Armor Cards/generate-armors.rb
+++ tactile_tabletop/Armor Cards/generate-armors.rb
@@ -1,13 +1,18 @@
 require 'squib'
 require 'game_icons'
 
-data = Squib.csv file: 'Tactile Tabletop Data - Armor.csv'
+if ARGV[0].nil?
+  data = Squib.csv file: 'Tactile_Tabletop_Data-Armors.csv'
+else
+  data = Squib.csv file: ARGV[0]
+end
 #grabbing icons from https://game-icons.net/
 #using gem game_icons to be able to load them
 
 
 #width/height/dpi measurements provided by template from BoardGameMaker.com, see poker-size.pdf
-Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].size, layout: 'armorcardlayout.yml')  do
+#45mm is equivalent to 530px
+Squib::Deck.new(dpi: 300, width: '45mm', height: '45mm', cards: data['Armor Name'].size, layout: 'armorcardlayout.yml')  do
 
   ## overall card stuff
 
@@ -25,7 +30,7 @@ Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].siz
 
   save_png prefix: 'ttac_'
   #save_pdf trim: 37.5
-  save_sheet prefix: 'ttac-print_', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
+  save_sheet prefix: 'armor_sprue_', sprue: 'letter_poker_card_armors.yml'
 end
 
 Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].size, layout: 'armorcardlayout.yml')  do
@@ -40,5 +45,5 @@ Squib::Deck.new(dpi: 300, width: 530, height: 530, cards: data['Armor Name'].siz
 
   save_png prefix: 'ttac_BACK'
   #save_pdf trim: 37.5
-  save_sheet prefix: 'ttac-print_BACK', columns: 3, rows: 5, margin: 0, gap: 5, trim: 35
+  save_sheet prefix: 'armor_back_sprue_', sprue: 'letter_poker_card_armors.yml'
 end
